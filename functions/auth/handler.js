227a48db7e0043e6e84cba249b2b28dbaae8c207
abalone0204 <abalone0204@gmail.com>
2016-06-06 17:01:17 +0800

    set endpoint url and add CORS plugin, then change the response data type of auth

--- functions/auth/handler.js
+++ functions/auth/handler.js
@@ -1,19 +1,17 @@
 'use strict'
 
+const endpointURI = 'https://d2xhcc9852.execute-api.ap-northeast-1.amazonaws.com/dev'
 
 function getAccessTokenUrl(event) {
     const params = {
             appId: event.app_id,
-            redirectURI: `${endpointURI}${event.path}`,
+            redirectURI: `${event.endpoint_url}/${event.stage}/${event.path}`,
             secret: event.secret,
             code: event.code,
             ip: event.ip
         }
     return `https://graph.facebook.com/v2.3/oauth/access_token?client_id=${params.appId}&redirect_uri=${params.redirectURI}&client_secret=${params.secret}&code=${params.code}`
 }
-
-
-const endpointURI = 'http://localhost:3000'
 
 module.exports.handler = function(event, context) {
     if (event.code) {
@@ -22,7 +20,7 @@ module.exports.handler = function(event, context) {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                context.succeed(JSON.stringify(data, null, 2))
+                context.succeed(data)
             })
     } else {
         context.fail(new Error('code not found'))
--- functions/callback/handler.js
+++ functions/callback/handler.js
@@ -2,6 +2,7 @@
 
 module.exports.handler = function(event, context) {
     console.log('event ==>', event);
+    console.log('context ==> ', context);
     if (!!event.code) {
         context.succeed('redirecting')
     } else {
